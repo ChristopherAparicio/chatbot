0dd43f17bcec1ef6588a20056a2f952af54a8b82
Christopher Aparicio <caparicio@caparicio-ChatBotVM.vm.vdi.s1.p.fti.net>
2017-05-12 16:04:14 +0200

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.post('/hello',function(req,res){
+	return res.json({
+		speech:"Hello",
+		displayText:"World",
+		source:"Webhook"
+	})
+})
+
+app.listen((process.env.PORT || 8000),function(){
+	console.log('Server ready on', (process.env.PORT || 8000));
+});
